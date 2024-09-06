6054adb4a42a96275804c0a14e279669ac6bb60d
Al-Amin <alamin50cse.miu@gmail.com>
2024-09-06 21:37:31 +0600

    card icon add

--- src/Components/PriceOption.jsx
+++ src/Components/PriceOption.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import Feature from "./Feature";
+import { GoPackageDependents } from "react-icons/go";
 
 const PriceOption = ({ option }) => {
   const { price, name, features } = option;
 
   return (
     <div className="flex flex-col bg-blue-700 text-white rounded-lg py-2 px-2">
-      <h1>
+      <h1 className="text-center mb-4">
         <span className="text-3xl">{price}</span>
         <span>/Mon</span>
       </h1>
@@ -16,8 +17,8 @@ const PriceOption = ({ option }) => {
           <Feature key={index} feature={feature}></Feature>
         ))}
       </div>
-      <button className="bg-green-600 hover:bg-lime-600 w-full rounded-lg mt-4 mb-2">
-        buy now
+      <button className="bg-green-600 hover:bg-lime-600 w-full rounded-lg mt-4 mb-2 py-2 font-bold flex flex-row items-center justify-center">
+        <GoPackageDependents className="mr-1"></GoPackageDependents> Buy now
       </button>
     </div>
   );
